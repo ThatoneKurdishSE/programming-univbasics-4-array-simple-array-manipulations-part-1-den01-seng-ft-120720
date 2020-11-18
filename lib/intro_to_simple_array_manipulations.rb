17b8bdb286edc9c66183c9012f71f5c77d0d16f3
Raz Al-Jaf <razaljaf@gmail.com>
2020-11-18 01:36:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,8 +13,7 @@ def using_unshift(bouroughs_in_nyc, string)
   end
   def pop_with_args(dog_breeds)
     dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
-    dog_breeds.pop.(2)
-    dog_breeds
+    dog_breeds.pop(2)
   end
   def using_shift(my_favorite_cities)
     my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
@@ -22,5 +21,5 @@ def using_unshift(bouroughs_in_nyc, string)
   end
   def shift_with_args(ice_cream_brands)
     ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
-    ice_cream_brands.shift=(2)
+    ice_cream_brands.shift(2)
   end
