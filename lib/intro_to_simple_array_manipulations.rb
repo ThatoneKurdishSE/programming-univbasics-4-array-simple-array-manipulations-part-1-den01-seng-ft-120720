2a1ab5316bbc059a580ac9048728ca4c78ae30ac
Raz Al-Jaf <razaljaf@gmail.com>
2020-11-18 01:38:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,5 +23,6 @@ def using_unshift(bouroughs_in_nyc, string)
   def shift_with_args(ice_cream_brands)
     ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
     ice_cream_brands.shift(2)
-    ice_cream_brands.size
+    
+    ice_cream_brands.size[]
   end
