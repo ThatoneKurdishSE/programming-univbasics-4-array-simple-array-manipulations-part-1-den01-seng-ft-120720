77f9440b3e3f07975e837ebd49fda76fbfba0d26
Raz Al-Jaf <razaljaf@gmail.com>
2020-11-18 02:11:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,10 +13,8 @@ def using_unshift(some_array, some_string)
     dog_breeds.pop(2)
   end
   def using_shift(my_favorite_cities)
-    my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
     my_favorite_cities.shift
   end
   def shift_with_args(ice_cream_brands)
-    ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
     ice_cream_brands.shift(2)
   end
