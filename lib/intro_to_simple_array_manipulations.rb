02b79c626f60532bf00722e2cc785b26b802d25d
Raz Al-Jaf <razaljaf@gmail.com>
2020-11-18 01:41:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -9,6 +9,7 @@ def using_unshift(bouroughs_in_nyc, string)
   def using_pop(continents)
     continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
     continents.pop
+     deleted_string="Antarctica"
   end
   def pop_with_args(dog_breeds)
     dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
